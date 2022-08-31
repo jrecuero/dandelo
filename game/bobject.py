a5d044987a1b1bddb4676da9c3c77d9fa17c9710
Jose Carlos Recuero <jose.recuero@gmail.com>
2022-08-31 16:50:24 -0700

    Add board_to_screen attribute.

--- game/bobject.py
+++ game/bobject.py
@@ -2,6 +2,7 @@
 in a game board.
 """
 
+from ast import Nonlocal
 import pygame
 import iobject
 
@@ -15,6 +16,11 @@ class BObject(iobject.IObject):
         """___init__ method creates a new BObject instance.
 
         - position attribute stores the X and Y position in the board.
+
+        - board_to_screen function translate a board position to a graphical
+        position. It is only required to be used if the board object contains
+        an sprite.
         """
         super().__init__(kwargs.get("the_name", None), kwargs.get("the_sprite", None))
         self.position = kwargs.get("the_position", pygame.Vector2())
+        self.board_to_screen = kwargs.get("the_board_to_screen", None)
